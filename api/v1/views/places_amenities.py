c401b19029a3a6c005d4c28ed91423725f3236d6
mahmoud94ha <mahmoudhannani3@gmail.com>
2023-06-25 00:37:32 +0100

    add

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -16,7 +16,7 @@ def amenities_per_place(place_id=None):
     try:
         place = storage.get('Place', place_id)
     except AttributeError:
-        return jsonify([])
+        abort(404, 'Not found')
 
     if request.method == 'GET':
         amenities_all = storage.all('Amenity')
@@ -38,7 +38,7 @@ def amenity_to_place(place_id=None, amenity_id=None):
     try:
         place = storage.get('Place', place_id)
     except AttributeError:
-        return jsonify([])
+        abort(404, 'Not found')
 
     amenity_obj = storage.get('Amenity', amenity_id)
     if amenity_obj is None:
