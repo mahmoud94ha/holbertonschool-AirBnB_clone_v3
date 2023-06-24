dc63a714bea408709fa3195ee0205c36020a7135
mahmoud94ha <mahmoudhannani3@gmail.com>
2023-06-24 04:07:35 +0100

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,23 @@ Contains the api app.py
 """
 from api import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """get status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of each object by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
