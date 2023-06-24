4792add381716f24d3e2ac53e994c50414f9efb8
mahmoud94ha <mahmoudhannani3@gmail.com>
2023-06-24 04:04:46 +0100

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ Contains the api app.py
 from api import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """get status"""
