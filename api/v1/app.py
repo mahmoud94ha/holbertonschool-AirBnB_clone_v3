86ba88010c7323800c22f459669d2f2da6fc33eb
mahmoud94ha <mahmoudhannani3@gmail.com>
2023-06-24 05:40:29 +0100

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,15 @@ Contains the api app.py
 """
 import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
