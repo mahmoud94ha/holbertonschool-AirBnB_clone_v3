3697b11544805ddd01dec96b77d7bbcb70c2f593
mahmoud94ha <mahmoudhannani3@gmail.com>
2023-06-24 04:11:21 +0100

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,12 @@ Contains the api app.py
 from api import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -12,15 +18,15 @@ def get_status():
     """get status"""
     return jsonify({"status": "OK"})
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    """Retrieves the number of each object by type"""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+@app_views.route('/stats')
+def show_stats():
+    """returns the count for all objects"""
+    all_counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
-    return jsonify(stats)
+    return jsonify(all_counts)
